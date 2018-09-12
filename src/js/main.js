dca3ef74f1a7e8f17237d3be0a55533b6b765212
Stanislava Sugovdz <sugovdz.s90@gmail.com>
2018-09-12 23:59:57 +0300

    Created markup for footer.

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,29 @@
-const onToggleMenuPressed = () => {
-    const menu = document.querySelector('.header-menu');
-    document.querySelector('body').classList.toggle('no-scroll')
-    return menu.classList.toggle('active')
+
+
+// const onToggleMenuPressed = () => {
+//     const menu = document.querySelector('.header-menu');
+//     document.querySelector('body').classList.toggle('no-scroll')
+//     return menu.classList.toggle('active')
+// };
+
+const hamburger = document.querySelector('.header-menu-toggle');
+const menu = document.querySelector('.header-menu');
+
+const toggleMenu = function toggleMenu() {
+    menu.classList.toggle('active');
 };
+
+hamburger.addEventListener('click', function(e) {
+    e.stopPropagation();
+    toggleMenu();
+});
+
+document.addEventListener('click', function (e) {
+    let target = e.target;
+    let its_menu = target == menu || menu.contains(target);
+    let its_hamburger = target == hamburger;
+    let menu_is_active = menu.classList.contains('active');
+    if (!its_menu && !its_hamburger && menu_is_active) {
+        toggleMenu();
+    }
+});
